987fa940d8f3341237cfea97d46d0edd7245a8bb
zlederma <zlederma@ucsd.edu>
2022-08-25 21:48:53 -0500

    Getting close, need to work on json structure

--- src/utils/CaseStudyComponent.js
+++ src/utils/CaseStudyComponent.js
@@ -3,6 +3,7 @@ import { getCaseStudy } from './case-study-fetcher';
 import React from 'react';
 import ReactHtmlParser from 'react-html-parser';
 import { useState, useEffect } from "react";
+import Iframe from 'react-iframe';
 
 const showHero = (data) => {
     return (
@@ -16,42 +17,96 @@ const showHero = (data) => {
     )
 }
 const showElement = (elementData) => {
-
+    if (elementData.type === "P") {
+        return (
+            <p className="mb-4 paragraph">
+                {ReactHtmlParser(elementData.value)}
+            </p>
+        )
+    }
+    if (elementData.type === "I") {
+        return (
+            <Image
+                className={elementData.class}
+                src={elementData.value} />
+        )
+    }
+    if (elementData.type === "F") {
+        return (
+            <Iframe
+                className={elementData.class}
+                src={elementData.value}
+                allowFullScreen />
+        )
+    }
 }
 
 const showElements = (elementsData) => {
-
+    let elements = [];
+    for (let i = 0; i < elementsData.length; i++) {
+        elements.push(showElement(elementsData[i]));
+    }
+    return elements;
 }
-const showCol = (colData) => {
 
+const showCol = (colData) => {
+    // console.log(colData)
+    return (
+        <Col lg={colData.lg} sm={colData.sm} xs={colData.xs}>
+            {showElements(colData.elem)}
+        </Col>
+    )
 }
 
 const showCols = (colsData) => {
+    // console.log("here")
+    // console.log(colsData)
+    let cols = [];
+    for (let i = 0; i < colsData.length; i++) {
+        cols.push(showCol(colsData[i]));
+    }
+    return cols;
 
 }
 
 const showRow = (rowData) => {
+    console.log(rowData)
     return (
         <Row>
-            {/* {showCols(rowData)} */}
+            {showCols(rowData.col)}
         </Row>
     )
 }
 
 const showRows = (rowsData) => {
-
+    let rows = [];
+    for (let i = 0; i < rowsData.length; i++) {
+        rows.push(showRow(rowsData[i]));
+    }
+    return rows;
 }
 
 const showSection = (sectionData) => {
+    return (
+        <>
+            {/* Add something for heading and something for subheading */}
+            <h2 className="mb-2 mt-5 heading"> {sectionData.headingText} </h2>
+            <div>
+                {showRows(sectionData.row)}
+            </div>
+        </>
+    )
 
 }
 
 const showSections = (sectionsData) => {
+    let sections = [];
+    for (let i = 0; i < sectionsData.length; i++) {
+        sections.push(showSection(sectionsData.section[i]))
+    }
+    return sections;
 
 }
-
-
-
 
 export const CaseStudyComponent = (slug) => {
     const [page, setPage] = useState(null);
@@ -59,18 +114,19 @@ export const CaseStudyComponent = (slug) => {
         getCaseStudy(slug).then(data => setPage(data))
     }, []);
     if (page !== null) {
-        console.log(page);
+        // console.log(page);
     }
     if (page !== null) {
         const heroData = page.caseStudyCollection.items[0].hero;
         const sectionsData = page.caseStudyCollection.items[0].sections;
-        console.log(sectionsData);
-        console.log(sectionsData.section[1].row[1]);
+        // console.log(sectionsData);
+        // console.log(sectionsData.section[1].row[1]);
         return (
             <div className="body">
                 <div className="mx-4 container">
                     <div style={{ minHeight: "100px" }}></div>
                     {showHero(heroData)}
+                    {showSection(sectionsData.section[0])}
                     {/* {showSections(sectionsData)} */}
                     <div style={{ minHeight: "80px" }}></div>
                 </div>
