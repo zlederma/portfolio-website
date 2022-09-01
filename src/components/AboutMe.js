5a4659b242086b0877d428fc86c38cfee938a5d7
zlederma <zlederma@ucsd.edu>
2022-08-31 22:25:38 -0500

    Made some copy changes

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -35,33 +35,19 @@ export default function AboutMe() {
                         <Row>
                             <Col lg={6} sm={12} xs={12}>
                                 <p className={pClass} style={pStyle}>
-                                    I’m a Junior at UC: San Diego, a New Mexican, a rock climber, a cat fanatic, a book lover. I’m also a Front-End Developer and User Interface Designer. </p>
+                                    Determined, fast-learning and detail oriented UC: San Diego graduate with a degree in Cognitive Science (Design and Interaction) and a minor in Computer Science. I’m also a rock climber, a cat fanatic, and a book lover. Former REI Frontend Software Developer Intern, who is now seeking full time development roles.  </p>
 
                                 <p className={pClass} style={pStyle}>
-                                    As a developer I am experienced in using Javascript, React, and Bootstrap to create dynamic and responsive web applications. I am also interested in learning to utilize new languages and tools to become a better developer. </p>
+                                    As a developer I am experienced in using frontend technologies such as Javascript, React, Vue, and Bootstrap to create dynamic and responsive web applications. I am also interested in learning to utilize new languages and tools to become a better developer. While I am both a developer and a designer, I am more focused on the development process.  </p>
 
                                 <p className={pClass} style={pStyle}>
-                                    Human centered design is especially important in an increasingly digital world. This is one of the aspects of design I am passionate about. Taking what I have learned about how humans think; such as how we process information, cognitive biases, and heuristics, in order to create a user-friendly innovative design. I am good at connecting ideas and looking at the whole picture in a design challenge, while still remaining detail-oriented. </p>
-
-                                <p className={pClass} style={pStyle}>
-                                    Although I am both a developer and a designer, my main focus is on the development process.
-
-                                </p>
+                                    Human centered design is especially important in an increasingly digital world. This is one of the aspects of design I am passionate about. Taking what I have learned about how humans think; such as how we process information, cognitive biases, and heuristics, in order to create a user-friendly design. I am good at connecting ideas and looking at the whole picture in a design challenge, while still remaining detail-oriented.</p>
                             </Col>
                             <Col lg={6} sm={6} xs={6}>
                                 <Image className={imgClass} src={climbingPhoto} style={{ width: "100%" }} />
                             </Col>
                         </Row>
                     </div>
-                    <h2 className={h2Class} style={h2Style}> Programming Process</h2>
-                    <p className={pClass} style={pStyle}>
-                        When I’m working on a project it is inevitable that I will face complications, so having a process to face these obstacles is important.
-                        My philosophy towards programming is very circular and includes a lot of inputs and outputs. I am constantly learning new things, applying
-                        some of the things I learned to the current project I’m working on, getting stuck on something else (or the same thing),
-                        going back to refine my understanding of the topic or learn a new topic, and the cycle continues. I personally find this process
-                        very satisfying and it is one of the things I love about programming. This circular process also allows me to constantly
-                        update my understanding of a topic and get better at it, while simultaneously developing new skills.
-                    </p>
                     <div style={{ minHeight: "80px" }}></div>
                 </div>
             </div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
         <div style={{ border: "1px solid lightgray", minWidth: "100vw", backgroundColor: "#f5eae9", display: "flex", justifyContent: "center", alignContent: "center" }}>
             <div className="mx-4" style={{ maxWidth: "1100px" }}>
                 <div style={{ height: "10px" }}></div>
-                <p> &copy; Zoe Lederman 2021</p>
+                <p> &copy; Zoe Lederman 2022</p>
             </div>
         </div >
     )
