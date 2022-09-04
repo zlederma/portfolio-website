cd574aaaadf70089da0a5483c1b7a41ab6634697
zlederma <zlederma@ucsd.edu>
2022-09-04 13:47:52 -0500

    started initial setting up of fetching project card data from CMS

--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -6,6 +6,8 @@ import portfolio_ception_logo from '../assets/portfolio_ception_logo.jpg'
 import triton_jam_logo from '../assets/triton-jam-logo.png'
 import ProjectCard from './ProjectCard'
 import "./CardStyles.css"
+import { getProjectCards } from '../utils/project-cards-fetcher'
+import { useState, useEffect } from "react"
 
 //Change this to be controlled from CMS
 const cardTitles = ["Triton Jam", "Temperature Blanket", "Instacart Redesign", "Portfolioception"];
@@ -13,21 +15,32 @@ const images = [triton_jam_logo, blanketLogo, instacartLogo, portfolio_ception_l
 const links = ["/projects/triton-jam", "/projects/temperature-blanket", "/projects/instacart-redesign", "/projects/portfolioception"]
 
 export default function ProjectCards() {
+    const [page, setPage] = useState(null);
+    useEffect(() => {
+        getProjectCards().then(data => setPage(data))
+    }, []);
+    // console.clear();
+    if (page !== null) {
+        // const heroData = page.caseStudyCollection.items[0].hero;
+        // const sectionsData = page.caseStudyCollection.items[0].sections;
+        console.log(page);
+    }
     return (
-        <div style={{ border: "1px solid lightgray" }}>
-            <div className="card__container">
-                <div className="p-4" style={{ width: "1100px" }}>
-                    <Row className="g-4" xs={1} md={1} lg={3}>
-                        {Array.from({ length: 4 }).map((_, idx) => (
-                            <Col>
-                                <ProjectCard link={links[idx]}
-                                    title={cardTitles[idx]}
-                                    image={images[idx]} />
-                            </Col>
-                        ))}
-                    </Row>
-                </div>
-            </div>
-        </div >
+        <div>test</div>
+        // <div style={{ border: "1px solid lightgray" }}>
+        //     <div className="card__container">
+        //         <div className="p-4" style={{ width: "1100px" }}>
+        //             <Row className="g-4" xs={1} md={1} lg={3}>
+        //                 {Array.from({ length: 4 }).map((_, idx) => (
+        //                     <Col>
+        //                         <ProjectCard link={links[idx]}
+        //                             title={cardTitles[idx]}
+        //                             image={images[idx]} />
+        //                     </Col>
+        //                 ))}
+        //             </Row>
+        //         </div>
+        //     </div>
+        // </div >
     )
 }
---
+++ src/utils/project-cards-fetcher.js
@@ -0,0 +1,33 @@
+//Slug is the unique id on each case study in contentful
+export const getProjectCards = () => {
+    // console.log(slug.slug);
+
+    const query = `
+  {
+      cardsCollection(where: { slug: "master" }) {
+        items {
+          cards
+        }
+      }
+    }
+    `
+    console.log(query);
+    return (window
+        .fetch(`https://graphql.contentful.com/content/v1/spaces/${process.env.REACT_APP_SPACE_ID}/`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                // Authenticate the request
+                Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
+            },
+            // send the GraphQL query
+            body: JSON.stringify({ query }),
+        })
+        .then((response) => response.json())
+        .then(({ data, errors }) => {
+            if (errors) {
+                console.error(errors);
+            }
+            return data;
+        }))
+}
