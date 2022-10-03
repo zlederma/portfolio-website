76e2b105fbc5bcd2f3c49d929ae891e25c717557
zlederma <zlederma@ucsd.edu>
2022-10-02 21:15:08 -0500

    Starting seperating the css

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,20 +1,11 @@
-import React from 'react'
 import background from '../assets/home-background-3.png'
+import './WelcomeStyles.css'
+
 
 export default function Welcome() {
     return (
 
-        <div style={{
-            minWidth: "100vw",
-            height: "65vh",
-            backgroundImage: `url(${background})`,
-            backgroundPosition: 'right top',
-            backgroundSize: "contain",
-            backgroundAttachment: "fixed",
-
-            backgroundRepeat: 'no-repeat',
-        }}>
-
+        <div className='welcome__container'>
             <div style={{ position: "sticky", top: "100px" }}>
                 <div className="mx-4" style={{ border: "2px solid rgba(0,0,0,0)" }}>
                     <h1 className="Hello" style={{ fontFamily: "Roboto", fontSize: "60px", fontWeight: "900", letterSpacing: "0px", lineHeight: "60px" }}> Hi It's Zoe,</h1>
