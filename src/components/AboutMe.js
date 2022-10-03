f8a9338d91ad380887a35d45fcfb1fef098bf7b0
zlederma <zlederma@ucsd.edu>
2022-10-03 11:47:20 -0500

    Fixed the section href

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,20 +4,20 @@ import './AboutMeStyles.css'
 
 export default function AboutMe() {
     return (
-        <div className="about-me__wrapper">
+        <div id="About" className="about-me__wrapper">
             <div className="about-me__container">
                 <h1 className='about-me__heading'> About Me </h1>
                 <div>
                     <Row>
                         <Col lg={6} sm={12} xs={12}>
                             <p>
-                                Determined, fast-learning and detail oriented UC: San Diego graduate with a degree in Cognitive Science (Design and Interaction) and a minor in Computer Science. I’m also a rock climber, a cat fanatic, and a book lover. Former REI Frontend Software Developer Intern, who is now seeking full time development roles.
+                                Determined, fast-learning and detail-oriented UC: San Diego graduate with a degree in Cognitive Science (Design and Interaction) and a minor in Computer Science. I’m also a rock climber, a cat fanatic, and book lover. Former REI Frontend Software Developer Intern, who is now seeking full time development roles!
                             </p>
                             <p>
-                                As a developer I am experienced in using frontend technologies such as Javascript, React, Vue, and Bootstrap to create dynamic and responsive web applications. I am also interested in learning to utilize new languages and tools to become a better developer. While I am both a developer and a designer, I am more focused on the development process.
+                                I have experience in using frontend technologies such as Javascript, React, and Vue to create dynamic and responsive web applications. I am fascinated by and invested in learning to utilize new languages and tools to become a better developer.
                             </p>
                             <p>
-                                Human centered design is especially important in an increasingly digital world. This is one of the aspects of design I am passionate about. Taking what I have learned about how humans think; such as how we process information, cognitive biases, and heuristics, in order to create a user-friendly design. I am good at connecting ideas and looking at the whole picture in a design challenge, while still remaining detail-oriented.
+                                I have a background in UI/UX Design - including user research, user testing and prototyping. While I am happy to bring this skill set to the table, development is where my passion lies. However, understanding UI/UX design gives me a head start in clearly and productively communicating with designers, along with helping me to succeed in the visual aspects of frontend development.
                             </p>
                         </Col>
                         <Col lg={6} sm={8} xs={12}>
