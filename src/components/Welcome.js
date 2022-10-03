4ff01bb6bf8f00aef1a3b7e147702db87b9bbe02
zlederma <zlederma@ucsd.edu>
2022-10-03 11:20:26 -0500

    cleaned up code

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,13 +3,15 @@ import './WelcomeStyles.css'
 export default function Welcome() {
     return (
 
-        <div className="welcome__container">
-            <div className="welcome__intro-container">
-                <div className="welcome__heading"> Hi It's Zoe,</div>
-                <div className="welcome__tagline">
-                    I'm a Frontend Developer
-                    <br />fully focused on figuring out how to <br />
-                    fabricate the finest experience
+        <div className="welcome__background-container">
+            <div className='welcome__container'>
+                <div className="welcome__intro-container">
+                    <div className="welcome__heading"> Hi, It's Zoe</div>
+                    <div className="welcome__tagline">
+                        I'm a Frontend Developer
+                        <br />fully focused on figuring out how to <br />
+                        fabricate the finest experience
+                    </div>
                 </div>
             </div>
         </div >
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,17 +6,10 @@ import AboutMe from '../components/AboutMe'
 
 export default function Home() {
     return (
-        <div>
-
-            <div style={{ display: "flex", justifyContent: "center" }}>
-                <div style={{ maxWidth: "1100px" }}>
-                    <Welcome></Welcome>
-                </div>
-            </div>
-
-
+        <>
+            <Welcome></Welcome>
             <ProjectCards slug="master" />
             <AboutMe></AboutMe>
-        </div>
+        </>
     )
 }
