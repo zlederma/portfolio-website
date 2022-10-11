bb8beb687840ad6b9c4e22177be3f164be55776c
zlederma <zlederma@ucsd.edu>
2022-10-11 18:36:05 -0500

    Initial creation

---
+++ src/components/Technologies.js
@@ -0,0 +1,17 @@
+import "./TechnologiesStyles.css"
+import Technology from "./Technology"
+export default function Technologies(props) {
+
+    const showTechnologies = (data) => {
+        let technologies = [];
+        for (let i = 0; i < data.length; i++) {
+            technologies.push(<Technology data={data[i]} />)
+        }
+        return technologies;
+    }
+    return (
+        <div className="technologies__container">
+            {showTechnologies(props.data)}
+        </div>
+    )
+}
