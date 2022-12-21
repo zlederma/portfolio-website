91dec8b5450a9a7cb753b68d075b968da1f4d6cd
zlederma <zlederma@ucsd.edu>
2022-12-21 13:00:40 -0600

    Succesfully added the sticky hello to my landing page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,24 +1,17 @@
-import ArtSquares from './ArtSquares'
-import ProjectCards from './ProjectCards'
 import './WelcomeStyles.css'
 export default function Welcome() {
     return (
-        <>
-            <div className="welcome__background-container">
-                <ArtSquares />
-                <div className='welcome__container'>
-                    <div className="welcome__intro-container">
-                        <div className="welcome__heading"> Hi, It's Zoe</div>
-                        <div className="welcome__tagline">
-                            I'm a Frontend Developer
-                            <br />fully focused on figuring out how to <br />
-                            fabricate the finest experience
-                        </div>
-                    </div>
+        <div className='welcome__container'>
+            <div className="welcome__intro-container">
+                <div className="welcome__heading"> Hi, It's Zoe</div>
+                <div className="welcome__tagline">
+                    I'm a Frontend Developer
+                    <br />fully focused on figuring out how to <br />
+                    fabricate the finest experience
                 </div>
-            </div >
-            <ProjectCards slug="master" use="home" />
-        </>
+            </div>
+        </div>
+
 
     )
 }
