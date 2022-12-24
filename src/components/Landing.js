0c85f9d929cf8c3d6c1a3bb3331bba07c0b28d7a
zlederma <zlederma@ucsd.edu>
2022-12-24 14:38:01 -0600

    got rid of extraneous code

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,17 +1,19 @@
 import "../styles/LandingStyles.css"
 import ProjectCards from "./ProjectCards"
 import Welcome from "./Welcome"
+import ArtSquares from "./ArtSquares"
 export default function () {
     return (
         <>
             <div className="landing__container">
-                <div className="landing__column-art"></div>
+                <div className="landing__column-art">
+                    <ArtSquares />
+                </div>
                 <div className="landing__column-welcome">
                     <Welcome />
                 </div>
                 <div className="landing__column-projects"></div>
             </div>
-            <ProjectCards slug="master" />
         </>
     )
 }
