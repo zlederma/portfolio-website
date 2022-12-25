ee13281fe5cdb6215f858650efe157b3b7939e26
zlederma <zlederma@ucsd.edu>
2022-12-24 21:52:06 -0600

    feat: maps each bracket to it's color onMouseEnter

--- src/components/ArtSquare.js
+++ src/components/ArtSquare.js
@@ -1,17 +1,47 @@
 import '../styles/ArtSquareStyles.css'
 import { RiBracesFill, RiCodeFill, RiBracketsFill, RiParenthesesFill } from "react-icons/ri";
+import { useState } from 'react';
 export default function ArtSquare({ bracket }) {
-    function renderBracket() {
-        const className = "art-square__square";
+
+    const [fill, setFill] = useState("#fffaf1")
+    const handleMouseEnter = () => {
         switch (bracket) {
             case 0:
-                return <RiBracesFill className={className} />
+                setFill("#c5462c");
+                break;
             case 1:
-                return <RiCodeFill className={className} />
+                setFill("#008c6a");
+                break;
             case 2:
-                return <RiBracketsFill className={className} />
-            case 3:
-                return <RiParenthesesFill className={className} />
+                setFill("#9b4acf");
+                break;
+        }
+    };
+
+    const handleMouseLeave = () => {
+        setFill("#fffaf1");
+    };
+    function renderBracket() {
+        const className = "art-square__square";
+
+        switch (bracket) {
+            case 0:
+                return <RiBracesFill
+                    className={className}
+                    fill={fill} onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave} />
+            case 1:
+                return <RiCodeFill
+                    className={className}
+                    fill={fill} onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave} />
+            case 2:
+                return <RiBracketsFill
+                    className={className}
+                    fill={fill} onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave} />
+            // case 3:
+            //     return <RiParenthesesFill className={className} />
         }
     }
     return (
