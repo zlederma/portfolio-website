552a01341c4de1b0aa6e06c2bd919d1d09f84869
zlederma <zlederma@ucsd.edu>
2022-12-25 13:51:17 -0600

    started working on code for the new project cards

--- src/components/DetailedProjectCard.js
+++ src/components/DetailedProjectCard.js
@@ -6,6 +6,19 @@ export default function DetailedProjectCard({ title }) {
             <div className="detailed-project-card__title">
                 {title}
             </div>
+            <ul className="detailed-project-card__tags">
+                <li> frontend</li>
+            </ul>
+            <img className="detailed-project-card__image" src="" />
+            {/* <ul className="detailed-project-card__technologies">
+                Technologiees
+                <li> Bootstrap</li>
+                <li> JavaScript</li>
+            </ul> */}
+            <ul className="detailed-project-card__links">
+                <li> Repo</li>
+                <li> Project</li>
+            </ul>
         </div>
     )
 }
