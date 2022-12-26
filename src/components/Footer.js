558167c77a266d511319e321c1c4efea5061014b
zlederma <zlederma@ucsd.edu>
2022-12-26 13:44:16 -0600

    Added responsivity for smaller screens

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <div style={{ border: "1px solid lightgray", minWidth: "100vw", backgroundColor: "#fffaf1", display: "flex", justifyContent: "center", alignContent: "center" }}>
+        <div style={{ borderTop: "1px solid lightgray", minWidth: "100vw", backgroundColor: "#fffaf1", display: "flex", justifyContent: "center", alignContent: "center" }}>
             <div className="mx-4" style={{ maxWidth: "1100px" }}>
                 <div style={{ height: "10px" }}></div>
                 <p> &copy; Zoe Lederman 2022</p>
