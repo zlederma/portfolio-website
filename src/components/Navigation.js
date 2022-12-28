123800a5d7a517a26a2c38adb794d5ec4db9c7f3
zlederma <zlederma@ucsd.edu>
2022-12-28 10:10:02 -0600

    mvp redoing of the navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,5 @@
-//This is the navbar
-//Styling for colors is in App.css
 import "../styles/NavigationStyles.css"
 import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap"
-
-import React from 'react'
-import { FaLinkedin, FaGithub, } from "react-icons/fa";
-import { IoDocumentTextSharp } from "react-icons/io5";
-import resume from '../assets/Resume-Zoe-Lederman.pdf';
-import NameIcon from '../assets/NameIcon';
 import Links from "./Links";
 import Brand from "./Brand";
 
@@ -15,31 +7,23 @@ import Brand from "./Brand";
 export default function Navigation() {
     return (
         <div>
-            <Navbar id="navbar" style={{ background: "rgba(41, 34, 32, 1)" }} collapseOnSelect expand="md"
+            <Navbar id="navbar" style={{ background: "rgba(41, 34, 32, 1)" }} collapseOnSelect expand="sm"
                 variant="dark"
                 fixed="top">
                 <Container>
-                    <Navbar.Brand href="/"> <div className="name-icon-div"
-                        style={{ paddingTop: "2px", display: "inline-block", height: "40px", width: "40px", borderRadius: "50%" }}>
-                        <NameIcon /> </div></Navbar.Brand>
+                    <Brand />
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="/#About">About Me</Nav.Link>
                             <NavDropdown title="Projects" id="collasible-nav-dropdown">
-                                <NavDropdown.Item className="my-dropdown-item" href="/projects/book-app">Book App</NavDropdown.Item>
-                                <NavDropdown.Item className="my-dropdown-item" href="/projects/triton-jam">Triton Jam</NavDropdown.Item>
-                                <NavDropdown.Item className="my-dropdown-item" href="/projects/portfolioception">Portfolioception</NavDropdown.Item>
-                                <NavDropdown.Item className="my-dropdown-item" href="/projects/temperature-blanket">Temperature Blanket</NavDropdown.Item>
-                                <NavDropdown.Item className="my-dropdown-item" href="/projects/instacart-redesign">Instacart Redesign</NavDropdown.Item>
+                                <NavDropdown.Item href="/projects/book-app">Book App</NavDropdown.Item>
+                                <NavDropdown.Item href="/projects/triton-jam">Triton Jam</NavDropdown.Item>
+                                <NavDropdown.Item href="/projects/portfolioception">Portfolioception</NavDropdown.Item>
+                                <NavDropdown.Item href="/projects/temperature-blanket">Temperature Blanket</NavDropdown.Item>
+                                <NavDropdown.Item href="/projects/instacart-redesign">Instacart Redesign</NavDropdown.Item>
                             </NavDropdown>
                         </Nav>
                         <Links />
-                        {/* <Nav className="mx-3 flex-row" justify="true" >
-                            <Nav.Link href="https://www.linkedin.com/in/zoe-lederman" target="_blank"> <FaLinkedin className="icon" style={{ fontSize: "30px" }} /></Nav.Link>
-                            <Nav.Link href="https://www.github.com/zlederma" target="_blank"><FaGithub className="icon" style={{ fontSize: "30px" }} /></Nav.Link>
-                            <Nav.Link href={resume} target="_blank"><IoDocumentTextSharp className="icon" style={{ fontSize: "30px" }} /></Nav.Link>
-                        </Nav> */}
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
